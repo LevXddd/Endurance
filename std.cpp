1c035d9c1b089aa5f2a4ff156e6dff830eb8421b
Lev <levieltxd@gmail.com>
2026-07-22 01:10:04 +0400

    Add files via upload

---
+++ std.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+
+void something() { std::cout << "Hello! Accept my congratulations" << '\n'; }
+
+double getNumber() {
+  double value{};
+  std::cin >> value;
+  return value;
+}
+
+int main() {
+  something();
+  std::cout << "Write the first number (x): ";
+  double x{getNumber()};
+
+  std::cout << "Write the second number (y): ";
+  double y(getNumber());
+
+  if (y == 0) {
+    std::cout << "The proccess is cancelled, division on zero is unavailable";
+    return EXIT_FAILURE;
+
+  } else {
+    std::cout << "The division will be " << x / y;
+    return EXIT_SUCCESS;
+  }
+}
